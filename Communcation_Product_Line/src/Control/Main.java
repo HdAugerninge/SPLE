ec06d48e7f492e240724eab7072cb140ff467f85
Torben <winklertb@googlemail.com>
2015-04-25 12:43:48 +0200

    Erster Commit

--- Communcation_Product_Line/src/Control/Main.java
+++ Communcation_Product_Line/src/Control/Main.java
@@ -1,10 +1,63 @@
-package Control;
+/*package Control;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+}*/
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Box;
+
+
+public class UltimateGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UltimateGUI window = new UltimateGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UltimateGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Box horizontalBox = Box.createHorizontalBox();
+		
+		
+		
 	}
 
 }
