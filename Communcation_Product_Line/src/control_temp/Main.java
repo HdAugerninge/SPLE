c51cd6e9f0185335c5a12e293cc3abb552998593
Tobias (Laptop) <h3rr.der.augenringe@googlemail.com>
2015-04-26 20:46:44 +0200

    hatte package Control mit großbuchstaben angelegt. temporär gelöscht um problem zu regeln

---
+++ Communcation_Product_Line/src/control_temp/Main.java
@@ -0,0 +1,25 @@
+package control_temp;
+//package Gui;
+
+//import Gui;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub#
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui window = new Gui();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
