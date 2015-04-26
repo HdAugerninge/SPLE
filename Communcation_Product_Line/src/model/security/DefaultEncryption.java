a169354a12a76ab8b8ba699a3f3ed69c56ff1bb9
Tobias (Laptop) <h3rr.der.augenringe@googlemail.com>
2015-04-26 20:28:24 +0200

    + architektur mit merge

---
+++ Communcation_Product_Line/src/model/security/DefaultEncryption.java
@@ -0,0 +1,10 @@
+package model.security;
+
+/**
+ * Created by test on 26.04.2015.
+ */
+public class DefaultEncryption {
+
+    public String encrypt (String s){return s;}
+    public String decrypt (String s){return s;}
+}
