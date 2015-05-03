cf8616b618278cd278a9dd964c5d3fcb6d76ee15
dennis.menge <dennis.menge@it-objects.de>
2015-05-03 12:29:28 +0200

    TextMessage overrides toString for convenience

--- Communcation_Product_Line/src/Model/Message.java
+++ Communcation_Product_Line/src/Model/Message.java
@@ -6,9 +6,9 @@ import java.util.List;
 public abstract class Message implements Serializable {
 
 	private static final long serialVersionUID = 7979236396181547676L;
-	Person sender;
-	List<Person> receivers;
-	Object payload;
+	protected Person sender;
+	protected List<Person> receivers;
+	protected Object payload;
 	
 	public Message(Person sender, List<Person> list) {
 		this.sender = sender;
